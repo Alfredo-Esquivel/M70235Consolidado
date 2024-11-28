d2ffea45948c42b545948e354962a5ef3cc0dd13
Alfredo-Esquivel <alfredoesqestudio@gmail.com>
2024-11-27 23:25:30 -0300

    Actualizacion

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 // config/db.config.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('db_bootcamp', 'postgres', 'Lucas8', {
+const sequelize = new Sequelize('db_bootcamp', 'postgres', 'postgres', {
     host: 'localhost',
     port: 5432,
     dialect: 'postgres',
